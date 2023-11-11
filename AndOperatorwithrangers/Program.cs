9c5920a4574d0e4531b68f3f40761b7dd07d9804
MaarikaK <testmaarika@gmail.com>
2023-11-11 16:02:34 +0200

    Week5

---
+++ AndOperatorwithrangers/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//////////////0 (or below) - Freezing
+//////////////from 0 to 5 - cold
+//////////////from 5 to 10 - chilly
+//////////////from 10 to 15 - cool
+//////////////from 15 to 28 - warm
+//////////////above 28 - boiling hot
+
+
+Console.WriteLine("Enter the current temp:");
+int currenttemp = Int32.Parse(Console.ReadLine());
+
+if (currenttemp <= 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if (currenttemp > 0 && currenttemp < 5)
+{
+    Console.WriteLine("Cold");
+}
+else if (currenttemp >=5 && currenttemp < 10)
+{
+    Console.WriteLine("Chilly");
+}
+
+else if (currenttemp >=10 && currenttemp < 15)
+{
+    Console.WriteLine("Cool!");
+}
+else if (currenttemp >=15 && currenttemp < 28)
+{
+    Console.WriteLine("Warm!");
+}
+else
+{
+    Console.WriteLine("Boiling hot!");
+}
